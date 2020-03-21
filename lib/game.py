f8bfa0d539d94d01258fb57c34bce2c375fef4fe
Hisstori <browncryan@outlook.com>
2020-03-21 19:24:49 -0400

    Created Pseudocode

---
+++ lib/game.py
@@ -0,0 +1,25 @@
+#import random method to use for attacks
+import random 
+
+# from hero import hero
+# from monsters import trash
+# TODO create welcome landing as table of contents with more info
+### TODO G create an input to take you to an instructions script
+### TODO S create an input to start the game
+# start of the adventure
+# shuffles trash(weak) enemies
+# shuffles boss(strong) enemies
+# this variable will hold the hero total health points
+# welcome user to game and ask them to input thier name
+# TODO ask user to select hero armor:  light, medium, heavy
+# accept user name and use that name for remainder of game
+# TODO user will select a weapon type: sword, dagger, hammer
+# TODO weapon type will determine power/number of attacks
+# user moves forward with adventure and encounters monster
+# user will choose to fight or flee, if fight attack(), else lose -5HP(Health Points)
+# user will continue to fight through four stages making fight or flee decision
+# user will encounter one of three boss enemies
+# user HAS to fight the BOSS there will be no flee
+# game is won once user defeats boss
+# game is lost if user loses all health points
+# give user the option to restart the game from the beginning
