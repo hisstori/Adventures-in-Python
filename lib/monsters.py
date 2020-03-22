88946a7ec68ded842d8d44033e06c43419577956
Hisstori <browncryan@outlook.com>
2020-03-21 20:02:14 -0400

    Added monster enemies

---
+++ lib/monsters.py
@@ -0,0 +1,27 @@
+# an array of monster dictionaries
+
+monster = [{
+    "name": "Imp Scout",
+    "health": 2,
+    "attack": 1
+},
+{
+    "name": "Goblin Scout",
+    "health": 4,
+    "attack": 1
+},
+{
+    "name": "Goblin Thug",
+    "health": 5,
+    "attack": 2
+},
+{
+    "name": "Orc Berserker",
+    "health": 7,
+    "attack": 7
+},
+{
+    "name": "Ogre Warrior",
+    "health": 10,
+    "attack": 4
+}]
