eeaff4a9ee4fce5a175a75fb6d357694d9ef1fd2
Hisstori <browncryan@outlook.com>
2020-03-21 20:45:27 -0400

    Added attack options to test

--- lib/bosses.py
+++ lib/bosses.py
@@ -1,2 +1,25 @@
+import random 
 # an array of boss enemy dictionaries
 # will include boss name, helath points, and attack value
+
+def attack():
+    random.randint(2,5)
+    # print(random.randint(2,5))
+    random.randint(1,7)
+    # print(random.randint(1,7))
+
+boss = [{
+    "name": "Orc Berserker",
+    "health": 30,
+    "attack": attack()
+},{
+    "name": "Goblin Assassin",
+    "health": 24,
+    "attack": 3 * 3
+},{
+    "name": "Ogre Captain",
+    "health": 35,
+    "attack": 8
+}]
+
+# attack()
