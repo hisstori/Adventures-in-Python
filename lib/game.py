71161c4b7d7bd83d56beb9e49793775573658132
Hisstori <browncryan@outlook.com>
2020-08-11 17:34:45 -0400

    Testing

--- lib/game.py
+++ lib/game.py
@@ -6,7 +6,7 @@ from monsters import trash
 from bosses import bosses
 
 def prRed(skk): print("\033[91m {}\033[00m" .format(skk)) 
-def prGreen(skk): print("\033[92m {}\033[00m" .format(skk)) 
+def prGreen(skk): print("\033[92m {}\033[00m" .format(skk))
 def prYellow(skk): print("\033[93m {}\033[00m" .format(skk)) 
 def prLightPurple(skk): print("\033[94m {}\033[00m" .format(skk)) 
 def prPurple(skk): print("\033[95m {}\033[00m" .format(skk)) 
